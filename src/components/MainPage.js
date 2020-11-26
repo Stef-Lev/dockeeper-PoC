4548a4fe861f3a5b64090e653892d733cb97b63a
Stefanos Leventis <sigianas@yahoo.com>
2020-11-26 23:09:35 +0200

    started building proof of concept

---
+++ src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
+
+function MainPage() {
+    return (
+        <div>
+            <h1>Main Page</h1>
+        </div>
+    )
+}
+
+export default MainPage;
